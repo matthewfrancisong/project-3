1823cc5ade9a15ea4f613d79ee7ce9883fa31479
Ray Tham <RayWTham@gmail.com>
2017-11-06 20:22:42 +0800

    created main controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'main/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to:''
+  # root to:''
 end
