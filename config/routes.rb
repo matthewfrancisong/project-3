ff997a319d5c587458b2f54c521ac03d803c3961
Shui <shuiloong@gmail.com>
2017-11-07 14:03:39 +0800

    added devise & scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  get 'table/index'
+
   root 'main#index'
+  
+  get 'main/index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root to:''
--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106115839) do
+ActiveRecord::Schema.define(version: 20171107042312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-    t.string "title"
-    t.date "date"
-    t.time "time"
-    t.string "location"
-    t.integer "num_table"
-    t.integer "num_seat"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table "guests", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.boolean "RSVP"
-    t.integer "table"
-    t.boolean "check_in"
-    t.bigint "admin_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["admin_id"], name: "index_guests_on_admin_id"
-  end
-
-  add_foreign_key "guests", "admins"
 end
