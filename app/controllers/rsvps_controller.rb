cdae0716837324d82b9e61f482401f806b8f8607
Matthew Francis Ong <matthewfoys@gmail.com>
2017-11-13 20:02:11 +0800

    matt- first readme draft

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -6,14 +6,6 @@ class RsvpsController < ApplicationController
 
   def edit
     @rsvp =Guest.find(params[:id])
-    # if @guest.update(post_params)
-      # flash[:success] = "Thank you!"
-    #   redirect_to guests_path
-    # else
-    #   redirect_back fallback_location: root_path
-    # end
-    #
-    # redirect_to new_guest_path
   end
 
   def update
@@ -22,7 +14,7 @@ class RsvpsController < ApplicationController
     redirect_to rsvp_path
     flash[:alert] = "Your response has been recorded. Thank you!"
   end
-  
+
   private
   def post_params
   params.require(:guest).permit(:RSVP)
